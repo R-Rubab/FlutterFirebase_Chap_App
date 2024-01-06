addd042a12d95028e490b6757e808e33a3d74713
~coding~ <krubab623@gmail.com>
2024-01-06 13:23:49 +0500

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_chat_application_11/Screens/chat_screen.dart';
+import 'package:flutter_chat_application_11/Screens/home_screen.dart';
+import 'package:flutter_chat_application_11/auth/login.dart';
 import 'package:flutter_chat_application_11/auth/splash_screen.dart';
 import 'package:flutter_chat_application_11/firebase_options.dart';
 
@@ -23,22 +26,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter chat app',
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          // textTheme: const TextTheme(titleMedium: TextStyle(fontSize: 22)),
-          appBarTheme: const AppBarTheme(
-            centerTitle: true,
-            elevation: 10,
-            // iconTheme: IconThemeData(color: Colors.black),
-            titleTextStyle: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.normal,
-                fontSize: 29),
-            // backgroundColor: Colors.white,
-          )),
-      home: const SplashScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter chat app',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            // textTheme: const TextTheme(titleMedium: TextStyle(fontSize: 22)),
+            appBarTheme: const AppBarTheme(
+              centerTitle: true,
+              elevation: 10,
+              // iconTheme: IconThemeData(color: Colors.black),
+              titleTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 29),
+              // backgroundColor: Colors.white,
+            )),
+        // home: const SplashScreen(),
+        home: const LoginScreen());
   }
 }
